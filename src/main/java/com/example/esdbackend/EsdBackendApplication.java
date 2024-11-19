20fe4f91852795def4e1dab2c73a29ee6b7412bb
Subham <subhamsourav20000@gmail.com>
2024-11-19 23:47:49 +0530

    init

---
+++ src/main/java/com/example/esdbackend/EsdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.esdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsdBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsdBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/esdbackend/EsdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsdBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
